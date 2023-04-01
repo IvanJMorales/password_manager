d90863547250b0d04cec784dd513b644de8d4f6b
Ivan Morales <IvanJohnMorales@Gmail.com>
2023-04-01 17:54:16 -0400

    Added while loop

--- password_manager.py
+++ password_manager.py
@@ -1,1 +1,17 @@
-# Init commit
+# Ask user for master password
+master_pwd = input("What is the master password? ")
+
+
+# Start of program
+while True:
+    mode = input("Would you like to add a new password or view existing ines (view, add)? Press q to quit ")
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        continue
